3907567ba5f257753d3b1839e63fd26b6889c335
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-09-12 23:52:00 -0700

    September 13th, 2024 Patch

--- system/sentry.py
+++ system/sentry.py
@@ -21,9 +21,9 @@ CRASHES_DIR = "/data/crashes/"
 
 class SentryProject(Enum):
   # python project
-  SELFDRIVE = "https://b42f6e8bea596ec3d7dc1d9a80280027@o4507524429185024.ingest.us.sentry.io/4507524452057088"
+  SELFDRIVE = "https://5ad1714d27324c74a30f9c538bff3b8d@o4505034923769856.ingest.us.sentry.io/4505034930651136"
   # native project
-  SELFDRIVE_NATIVE = "https://b42f6e8bea596ec3d7dc1d9a80280027@o4507524429185024.ingest.us.sentry.io/4507524452057088"
+  SELFDRIVE_NATIVE = "https://5ad1714d27324c74a30f9c538bff3b8d@o4505034923769856.ingest.us.sentry.io/4505034930651136"
 
 
 def bind_user() -> None:
@@ -39,7 +39,7 @@ def capture_tmux(params) -> None:
 
     if lines:
       while True:
-        if sentry_pinged():
+        if is_url_pingable("https://sentry.io"):
           with sentry_sdk.configure_scope() as scope:
             bind_user()
             scope.set_extra("tmux_log", "\n".join(lines))
@@ -115,7 +115,7 @@ def capture_fingerprint(candidate, params, blocked=False):
   while True:
     if is_url_pingable("https://sentry.io"):
       with sentry_sdk.configure_scope() as scope:
-        scope.fingerprint = [candidate, HARDWARE.get_serial()]
+        scope.fingerprint = [HARDWARE.get_serial()]
         for label, key_values in matched_params.items():
           scope.set_extra(label, "\n".join([f"{k}: {v}" for k, v in key_values.items()]))
 
