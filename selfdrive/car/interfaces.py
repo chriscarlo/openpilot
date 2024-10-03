74128769a51c4e91db8f16307fda8f3c188eaec9
chriscarlo <chris.rimoldi@icloud.com>
2024-10-03 12:14:38 -0700

    Long tweaks

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -28,8 +28,8 @@ GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
 
 MAX_CTRL_SPEED = (V_CRUISE_MAX + 4) * CV.KPH_TO_MS
-ACCEL_MAX = 2.0
-ACCEL_MIN = -3.5
+ACCEL_MAX = 5.0
+ACCEL_MIN = -6.0
 FRICTION_THRESHOLD = 0.3
 
 NEURAL_PARAMS_PATH = os.path.join(BASEDIR, 'selfdrive/car/torque_data/neural_ff_weights.json')
