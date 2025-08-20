3c3a243a8d8b6ba09bf29717c2a6de6f250ab851
chriscarlo <chris.rimoldi@icloud.com>
2025-08-20 16:59:44 -0700

    ui: subscribe to gpsLocationExternal so RTI HUD arrows can render (bearing + lat/lon present)

--- selfdrive/ui/sunnypilot/ui.cc
+++ selfdrive/ui/sunnypilot/ui.cc
@@ -20,6 +20,7 @@ UIStateSP::UIStateSP(QObject *parent) : UIState(parent) {
     "wideRoadCameraState", "managerState", "selfdriveState", "longitudinalPlan",
     "modelManagerSP", "selfdriveStateSP", "longitudinalPlanSP", "backupManagerSP", "carControl", "liveMapDataSP",
     "rtiStateSP",  // RTI (Realtime Traffic Intelligence) state for threat display
+    "gpsLocationExternal",  // Needed for RTI arrow bearings (lat/lon + heading)
   });
 
   // update timer
